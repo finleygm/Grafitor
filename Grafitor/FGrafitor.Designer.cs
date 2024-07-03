d717201d22746f5e55192c2b303373626635e318
Finley Guerra <finley_1231@hotmail.com>
2024-07-03 14:25:48 -0400

    Agregar archivos de proyecto.

---
+++ Grafitor/FGrafitor.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Grafitor
+{
+    partial class FGrafitor
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picLienzo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picLienzo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picLienzo
+            // 
+            this.picLienzo.BackColor = System.Drawing.Color.White;
+            this.picLienzo.Location = new System.Drawing.Point(82, 75);
+            this.picLienzo.Name = "picLienzo";
+            this.picLienzo.Size = new System.Drawing.Size(757, 521);
+            this.picLienzo.TabIndex = 0;
+            this.picLienzo.TabStop = false;
+            this.picLienzo.MouseDown += new System.Windows.Forms.MouseEventHandler(this.picLienzo_MouseDown);
+            this.picLienzo.MouseMove += new System.Windows.Forms.MouseEventHandler(this.picLienzo_MouseMove);
+            this.picLienzo.MouseUp += new System.Windows.Forms.MouseEventHandler(this.picLienzo_MouseUp);
+            // 
+            // FGrafitor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1117, 708);
+            this.Controls.Add(this.picLienzo);
+            this.Name = "FGrafitor";
+            this.Text = "Grafitor";
+            ((System.ComponentModel.ISupportInitialize)(this.picLienzo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picLienzo;
+    }
+}
+
---
+++ Grafitor/FGrafitor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grafitor
+{
+    public partial class FGrafitor : Form
+    {
+        Color colorPincel;
+        Bitmap btLienzo;
+        Boolean mouseButton1Down = false;
+
+        public FGrafitor()
+        {
+            InitializeComponent();
+            colorPincel = Color.FromArgb(0, 0, 0);
+            btLienzo = new Bitmap(400, 300);
+           
+        }
+
+        private void picLienzo_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.mouseButton1Down = true;
+        }
+        private void picLienzo_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (this.mouseButton1Down)
+            {
+                int X = e.X;
+                int Y = e.Y;
+                if(X>= 0&& X<btLienzo.Width &&Y>=0 && Y < btLienzo.Height)
+                {
+                    btLienzo.SetPixel(X, Y, colorPincel);
+                    picLienzo.Image = btLienzo;
+                }
+                
+            }
+            
+        }
+
+        private void picLienzo_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.mouseButton1Down = false;
+        }
+    }
+}
